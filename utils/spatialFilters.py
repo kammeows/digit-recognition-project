54e88231d4ad87f20a4a4f2e23d88d5c62056390
Kamakshi <107478431+kammeows@users.noreply.github.com>
2025-04-20 12:29:12 +0530

    Update spatialFilters.py

--- utils/spatialFilters.py
+++ utils/spatialFilters.py
@@ -1,8 +1,3 @@
-# adaptive median and mean filter
-# unsharp masking
-# log
-# dog
-
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -28,25 +23,6 @@ def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.5, threshold=0):
 
     return sharpened
 
-# def adaptive_mean_filter(img, kernel_size=(3,3), max_kernel_size=(7,7)):
-#     pad_h = kernel_size[0] // 2
-#     pad_w = kernel_size[1] // 2
-
-#     padded_img = np.pad(img, ((pad_h, pad_h), (pad_w, pad_w)), mode='reflect')
-#     new_img = np.zeros(img.shape, dtype=np.uint8)
-
-#     rows, cols = img.shape
-
-#     for i in range(rows):
-#         for j in range(cols):
-#             # Extract kernel window from padded image
-#             window = padded_img[i:i + kernel_size[0], j:j + kernel_size[1]]
-#             mean_val = np.mean(window)
-#             new_img[i, j] = np.uint8(mean_val)
-
-#     return new_img
-
-
 def adaptive_mean_filter(img, base_kernel=(3, 3), max_kernel=(7, 7), var_thresh=100):
     h, w = img.shape
     pad = max_kernel[0] // 2
@@ -69,14 +45,3 @@ def adaptive_mean_filter(img, base_kernel=(3, 3), max_kernel=(7, 7), var_thresh=
             filtered_img[i - pad, j - pad] = np.uint8(mean_val)
 
     return filtered_img
-
-
-# def adaptive_mean_filter(img, kernel_size=(3,3), max_kernel_size=(7,7)):
-#     new_img = np.zeros(img.shape, dtype=np.uint8)
-#     img,pad_h,pad_w = pad_image(img, kernel_size)
-#     for i in range(pad_h, pad_h+img.shape[0]):
-#         for j in range(pad_w, pad_w+img.shape[1]):
-#             window = img[i-pad_h:i+pad_h+1, j-pad_w:j+pad_w+1]
-#             mean_val = np.mean(window)
-#             new_img[i-pad_h, j-pad_w] = np.uint8(mean_val)
-#     return new_img
