472ace820692bde0d71c848b3b9ca48d52bdb5a7
Kamakshi <107478431+kammeows@users.noreply.github.com>
2025-04-20 12:39:33 +0530

    Update mnistTest.py

--- mnistTest.py
+++ mnistTest.py
@@ -1,51 +1,3 @@
-# import numpy as np
-# import cv2
-# import joblib
-
-# mnist_svm = joblib.load('models/svm_mnist_model.pkl')
-# # mnist_svm_rbf = joblib.load('models/svm_mnist_model_rbf.pkl')
-
-# image = cv2.imread('images/threeDigital.jpg', cv2.IMREAD_GRAYSCALE)
-# image = cv2.resize(image, (500, 500), interpolation=cv2.INTER_LINEAR)
-
-# blurred = cv2.GaussianBlur(image, (5,5), 0) # to get rid of noise, but there isnt a lot of it in this image
-# # cv2.imshow('bllurred_Image', blurred)
-# canny = cv2.Canny(blurred, 50,200,255)
-
-# # extract the edges and make a bounding box around the digits
-# # we find contours first, filter out noise from it, and then draw the bounding boxrm
-
-# contours,_ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# boundaryBoxImage = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-# for cnt in contours:
-#     x,y,w,h = cv2.boundingRect(cnt)
-#     if w>10 and h>10:
-#         cv2.rectangle(boundaryBoxImage, (x,y), (x+w,y+h), (0,255,0),2)
-
-# cv2.imshow('Original image', image)
-# cv2.imshow('Image with boundary box', boundaryBoxImage)
-
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# digit_images = []
-# for cnt in contours:
-#     x, y, w, h = cv2.boundingRect(cnt)
-#     if w > 10 and h > 10:
-#         digit = image[y:y+h, x:x+w]
-
-#         resized_digit_28x28 = cv2.resize(digit, (28, 28), interpolation=cv2.INTER_AREA)
-#         inverted_digit_28x28 = cv2.bitwise_not(resized_digit_28x28)
-#         normalized_digit_28x28 = inverted_digit_28x28 / 255.0
-#         flat_digit_28x28 = normalized_digit_28x28.flatten().reshape(1, -1)
-
-#         prediction_mnist_svm = mnist_svm.predict(flat_digit_28x28)[0]
-#         print("Predicted (SVM - MNIST 28x28):", prediction_mnist_svm)
-
-#         cv2.waitKey(0)
-#         cv2.destroyAllWindows()
-
 import numpy as np
 import cv2
 import joblib
@@ -90,15 +42,6 @@ digit_images = []
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
     if w > 10 and h > 10:
-        # digit = image[y:y+h, x:x+w]
-
-        # resized_digit_28x28 = cv2.resize(digit, (28, 28), interpolation=cv2.INTER_AREA)
-        # inverted_digit_28x28 = cv2.bitwise_not(resized_digit_28x28)
-        # cv2.imshow("Resized Digit", inverted_digit_28x28)
-        # # cv2.imshow("Inverted Digit", inverted_digit_28x28)
-
-        # normalized_digit_28x28 = resized_digit_28x28 / 255.0
-        # flat_digit_28x28 = normalized_digit_28x28.flatten().reshape(1, -1)
 
         digit = image[y:y+h, x:x+w]
         resized = cv2.resize(digit, (20, 20), interpolation=cv2.INTER_AREA)
